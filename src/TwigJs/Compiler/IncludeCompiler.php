d6ad441018e43b68488a948952288c5bc8365f4b
Alexander Kurbatsky <alexander.kurbatsky@intexsys.com>
2016-10-13 16:51:35 +0300

    Fixing issue when node with given name does not exist

--- src/TwigJs/Compiler/IncludeCompiler.php
+++ src/TwigJs/Compiler/IncludeCompiler.php
@@ -66,7 +66,7 @@ class IncludeCompiler implements TypeCompilerInterface
         $compiler->isTemplateName = false;
 
         if (false === $node->getAttribute('only')) {
-            if (null === $node->getNode('variables')) {
+            if (!$node->hasNode('variables')) {
                 $compiler->raw('context');
             } else {
                 $compiler
